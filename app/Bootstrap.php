ec520bb99055250831802160acb405937a66edcb
Piotr Gasiorowski <WooDzu@users.noreply.github.com>
2014-04-27 13:03:42 +0100

    Create Bootstrap.php

---
+++ app/Bootstrap.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+	+------------------------------------------------------------------------+
+	| Phosphorum                                                             |
+	+------------------------------------------------------------------------+
+	| Copyright (c) 2013-2014 Phalcon Team and contributors                  |
+	+------------------------------------------------------------------------+
+	| This source file is subject to the New BSD License that is bundled     |
+	| with this package in the file docs/LICENSE.txt.                        |
+	|                                                                        |
+	| If you did not receive a copy of the license and are unable to         |
+	| obtain it through the world-wide-web, please send an email             |
+	| to license@phalconphp.com so we can send you a copy immediately.       |
+	+------------------------------------------------------------------------+
+*/
+
+namespace Phosphorum;
+
+use Engine\Bootstrap as EngineBootstrap;
+use Phalcon\DiInterface;
+use Phalcon\Events\Manager;
+
+/**
+ * Phosphorum Bootstrap.
+ *
+ * @category  PhalconEye
+ * @package   Phosphorum
+ * @author    Piotr Gasiorowski <p.gasiorowski@vipserv.org>
+ * @copyright 2013-2014 Phalcon Team
+ * @license   New BSD License
+ * @link      https://github.com/phalcon/forum
+ */
+class Bootstrap extends EngineBootstrap
+{
+    /**
+     * Current module name.
+     *
+     * @var string
+     */
+    protected $_moduleName = "Phosphorum";
+
+    /**
+     * Bootstrap construction.
+     *
+     * @param DiInterface $di Dependency injection.
+     * @param Manager     $em Events manager object.
+     */
+    public function __construct($di, $em)
+    {
+        parent::__construct($di, $em);
+
+        /**
+         * Attach this bootstrap for all application initialization events.
+         */
+        $em->attach('init', $this);
+    }
+}
